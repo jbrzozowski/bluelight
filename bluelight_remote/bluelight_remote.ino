39a421479653f522fc251261271df392c642d275
John Jason Brzozowski <jjmb@jjmb.com>
2018-01-28 11:09:31 -0500

    Commenting out duplicate function

--- bluelight_remote/bluelight_remote.ino
+++ bluelight_remote/bluelight_remote.ino
@@ -605,6 +605,7 @@ void listen() {
 
 // Input a value 0 to 255 to get a color value.
 // The colors are a transition r - g - b - back to r.
+/**
 uint32_t Wheel(byte WheelPos) {
   if(WheelPos < 85) {
    return strip.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
@@ -616,3 +617,4 @@ uint32_t Wheel(byte WheelPos) {
    return strip.Color(0, WheelPos * 3, 255 - WheelPos * 3);
   }
 }
+**/

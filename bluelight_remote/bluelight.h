a301a73a870eacab93d1d063f9ba60a8521d0812
John Jason Brzozowski <jjmb@jjmb.com>
2018-02-19 00:37:50 -0500

    Clean up.

--- bluelight_remote/bluelight.h
+++ bluelight_remote/bluelight.h
@@ -82,11 +82,54 @@ This paragraph must be included in any redistribution.
 
 // #include <Adafruit_NeoPixel.h> // Included in the main .ino file
 
-#define N_PIXELS  60  // Number of pixels in strand
-#define MIC_PIN   A0  // Microphone is attached to this analog pin
-#define LED_PIN    6  // NeoPixel LED strand is connected to this pin
-#define DC_OFFSET  0  // DC offset in mic signal - if unusure, leave 0
-#define NOISE     10  // Noise/hum/interference in mic signal
-#define SAMPLES   60  // Length of buffer for dynamic level adjustment
-#define TOP       (N_PIXELS + 2) // Allow dot to go slightly off scale
-#define PEAK_FALL 40  // Rate of peak falling dot
+/*=========================================================================
+    APPLICATION SETTINGS
+
+    FACTORYRESET_ENABLE       Perform a factory reset when running this sketch
+   
+                              Enabling this will put your Bluefruit LE module
+                              in a 'known good' state and clear any config
+                              data set in previous sketches or projects, so
+                              running this at least once is a good idea.
+   
+                              When deploying your project, however, you will
+                              want to disable factory reset by setting this
+                              value to 0.  If you are making changes to your
+                              Bluefruit LE device via AT commands, and those
+                              changes aren't persisting across resets, this
+                              is the reason why.  Factory reset will erase
+                              the non-volatile memory where config data is
+                              stored, setting it back to factory default
+                              values.
+       
+                              Some sketches that require you to bond to a
+                              central device (HID mouse, keyboard, etc.)
+                              won't work at all with this feature enabled
+                              since the factory reset will clear all of the
+                              bonding data stored on the chip, meaning the
+                              central device won't be able to reconnect.
+    MINIMUM_FIRMWARE_VERSION  Minimum firmware version to have some new features
+    MODE_LED_BEHAVIOUR        LED activity, valid options are
+                              "DISABLE" or "MODE" or "BLEUART" or
+                              "HWUART"  or "SPI"  or "MANUAL"
+    PIN                       Which pin on the Arduino is connected to the NeoPixels?
+    NUMPIXELS                 How many NeoPixels are attached to the Arduino?
+    -----------------------------------------------------------------------*/
+    #define FACTORYRESET_ENABLE         1
+    #define MINIMUM_FIRMWARE_VERSION    "0.6.6"
+    #define MODE_LED_BEHAVIOUR          "MODE"
+    #define LED_PIN                       6
+    #define BOARD_PIN                     13  // NeoPixel LED strand is connected to this pin
+    #define N_PIXELS                      91
+    #define MIC_PIN                       A1
+    // Added to support RGB LED strips
+    #define REDPIN                        11
+    #define GREENPIN                      9
+    #define BLUEPIN                       5
+    #define FADESPEED                     5     // make this higher to slow down
+    #define DC_OFFSET                     0  // DC offset in mic signal - if unusure, leave 0
+    #define NOISE                         10  // Noise/hum/interference in mic signal
+    #define SAMPLES                       60  // Length of buffer for dynamic level adjustment
+    #define TOP                           (N_PIXELS + 2) // Allow dot to go slightly off scale
+    #define PEAK_FALL                     40  // Rate of peak falling dot    
+/*=========================================================================*/

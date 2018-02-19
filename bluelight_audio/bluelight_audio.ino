c76178b427474dc0a92f3d60001815d50248ea07
John Jason Brzozowski <jjmb@jjmb.com>
2018-02-19 00:38:11 -0500

    Clean up.

--- bluelight_audio/bluelight_audio.ino
+++ bluelight_audio/bluelight_audio.ino
@@ -25,12 +25,13 @@ This paragraph must be included in any redistribution.
 
 #include <Adafruit_NeoPixel.h>
 
+#define DELAY 0
 #define N_PIXELS  91  // Number of pixels in strand
 #define MIC_PIN   A1  // Microphone is attached to this analog pin
 #define LED_PIN    6  // NeoPixel LED strand is connected to this pin
 #define DC_OFFSET  0  // DC offset in mic signal - if unusure, leave 0
 #define NOISE     10  // Noise/hum/interference in mic signal
-#define SAMPLES   30  // Length of buffer for dynamic level adjustment
+#define SAMPLES   90  // Length of buffer for dynamic level adjustment
 #define TOP       (N_PIXELS + 2) // Allow dot to go slightly off scale
 #define PEAK_FALL 40  // Rate of peak falling dot
 
@@ -44,83 +45,132 @@ int
   minLvlAvg = 0,      // For dynamic adjustment of graph low & high
   maxLvlAvg = 512;
 
-Adafruit_NeoPixel strip = Adafruit_NeoPixel(N_PIXELS, LED_PIN, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(N_PIXELS, LED_PIN, NEO_GRB);
+// Adafruit_BluefruitLE_SPI ble(BLUEFRUIT_SPI_CS, BLUEFRUIT_SPI_IRQ, BLUEFRUIT_SPI_RST);
 
 void setup() {
+  Serial.println("setup -> start");
+  // Configure pin for onboard LED blinking
+  // pinMode(13, OUTPUT);
 
-  // This is only needed on 5V Arduinos (Uno, Leonardo, etc.).
-  // Connect 3.3V to mic AND TO AREF ON ARDUINO and enable this
-  // line.  Audio samples are 'cleaner' at 3.3V.
-  // COMMENT OUT THIS LINE FOR 3.3V ARDUINOS (FLORA, ETC.):
-  //  analogReference(EXTERNAL);
+  // This initializes the NeoPixel library
+  strip.begin();
+  
+  for(uint8_t i=0; i<N_PIXELS; i++) {
+    strip.setPixelColor(i, strip.Color(0,0,0)); // off
+  }
+  
+  colorWipe(strip.Color(100, 100, 100), 20); // do a quick colorWipe to show that the pixels are all working, even before Bluefruit connection established
+  colorWipe(strip.Color(0, 0, 0), 20);
+
+  // Initialize all pixels to 'off'
+  strip.show();
+  // strip.begin();
+  // strip.setPixelColor(n, red, green, blue);  
+  // strip.show();
 
   memset(vol, 0, sizeof(vol));
-  strip.begin();
+  // strip.begin();
+  Serial.println("setup -> complete");
 }
 
 void loop() {
+  Serial.println("loop -> start");  
   uint8_t  i;
   uint16_t minLvl, maxLvl;
   int      n, height;
 
+  // Raw reading from mic
+  Serial.println(" reading mic");
+  n   = analogRead(MIC_PIN);
 
+  // Center on zero
+  Serial.println(" center on zero");
+  n   = abs(n - 512 - DC_OFFSET);
 
-  n   = analogRead(MIC_PIN);                        // Raw reading from mic
-  n   = abs(n - 512 - DC_OFFSET); // Center on zero
-  n   = (n <= NOISE) ? 0 : (n - NOISE);             // Remove noise/hum
-  lvl = ((lvl * 7) + n) >> 3;    // "Dampened" reading (else looks twitchy)
+  // Remove noise/hum
+  Serial.println(" remove noise/hum");
+  n   = (n <= NOISE) ? 0 : (n - NOISE);
+
+  // "Dampened" reading (else looks twitchy)  
+  Serial.println(" dampen");
+  lvl = ((lvl * 7) + n) >> 3;
 
   // Calculate bar height based on dynamic min/max levels (fixed point):
+  Serial.println(" setting height");
   height = TOP * (lvl - minLvlAvg) / (long)(maxLvlAvg - minLvlAvg);
 
-  if(height < 0L)       height = 0;      // Clip output
+  // Clip output
+  Serial.println(" clip output");
+  if(height < 0L)       height = 0;
   else if(height > TOP) height = TOP;
-  if(height > peak)     peak   = height; // Keep 'peak' dot at top
 
+  // Keep 'peak' dot at top  
+  Serial.println(" peak = height");
+  if(height > peak)     peak   = height;
 
   // Color pixels based on rainbow gradient
   for(i=0; i<N_PIXELS; i++) {
+    Serial.println("  setting pixel color");      
     if(i >= height)               strip.setPixelColor(i,   0,   0, 0);
     else strip.setPixelColor(i,Wheel(map(i,0,strip.numPixels()-1,30,150)));
-
   }
   
   // Draw peak dot
+  Serial.println(" draw peak dot");
   if(peak > 0 && peak <= N_PIXELS-1) strip.setPixelColor(peak,Wheel(map(peak,0,strip.numPixels()-1,30,150)));
 
-   strip.show(); // Update strip
+  // Update strip
+  Serial.println(" update strip");
+   strip.show();
 
-  // Every few frames, make the peak pixel drop by 1:
+  // Every few frames, make the peak pixel drop by 1
+  Serial.println("  ...fall rate");    
+  if(++dotCount >= PEAK_FALL) {
+    //fall rate
+    Serial.println(" fall rate...");
+    if(peak > 0) peak--;
+    dotCount = 0;
+  }
 
-    if(++dotCount >= PEAK_FALL) { //fall rate
+  // Save sample for dynamic leveling
+  Serial.println(" saving sample for dynamic leveling");
+  vol[volCount] = n;
 
-      if(peak > 0) peak--;
-      dotCount = 0;
-    }
-
-  vol[volCount] = n;                      // Save sample for dynamic leveling
-  if(++volCount >= SAMPLES) volCount = 0; // Advance/rollover sample counter
+  // Advance/rollover sample counter
+  Serial.println(" advance/rollover sample counter");
+  if(++volCount >= SAMPLES) volCount = 0;
 
   // Get volume range of prior frames
+  Serial.println(" get volume range");
   minLvl = maxLvl = vol[0];
   for(i=1; i<SAMPLES; i++) {
     if(vol[i] < minLvl)      minLvl = vol[i];
     else if(vol[i] > maxLvl) maxLvl = vol[i];
   }
-  // minLvl and maxLvl indicate the volume range over prior frames, used
-  // for vertically scaling the output graph (so it looks interesting
-  // regardless of volume level).  If they're too close together though
-  // (e.g. at very low volume levels) the graph becomes super coarse
-  // and 'jumpy'...so keep some minimum distance between them (this
-  // also lets the graph go to zero when no sound is playing):
+  
+  /**
+  minLvl and maxLvl indicate the volume range over prior frames, used
+  for vertically scaling the output graph (so it looks interesting
+  regardless of volume level).  If they're too close together though
+  (e.g. at very low volume levels) the graph becomes super coarse
+  and 'jumpy'...so keep some minimum distance between them (this
+  also lets the graph go to zero when no sound is playing):
+  **/
   if((maxLvl - minLvl) < TOP) maxLvl = minLvl + TOP;
-    minLvlAvg = (minLvlAvg * 63 + minLvl) >> 6; // Dampen min/max levels
-    maxLvlAvg = (maxLvlAvg * 63 + maxLvl) >> 6; // (fake rolling average)
+  // Dampen min/max levels
+  Serial.println(" dampen min/max levels");
+  minLvlAvg = (minLvlAvg * 63 + minLvl) >> 6;
+  // (fake rolling average)
+  Serial.println(" fake rolling average");
+  maxLvlAvg = (maxLvlAvg * 63 + maxLvl) >> 6;
+  Serial.println("loop -> end");      
 }
 
-// Input a value 0 to 255 to get a color value.
-// The colors are a transition r - g - b - back to r.
+// Input a value 0 to 255 to get a color value
+// The colors are a transition r - g - b - back to r
 uint32_t Wheel(byte WheelPos) {
+  Serial.println("wheel -> start");  
   if(WheelPos < 85) {
    return strip.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
   } else if(WheelPos < 170) {
@@ -130,4 +180,16 @@ uint32_t Wheel(byte WheelPos) {
    WheelPos -= 170;
    return strip.Color(0, WheelPos * 3, 255 - WheelPos * 3);
   }
+  Serial.println("wheel -> end");
+}
+
+// Fill the dots one after the other with a color
+void colorWipe(uint32_t c, uint8_t wait) {
+  Serial.println("color wipe -> start");
+  for(uint16_t i=0; i<strip.numPixels(); i++) {
+      strip.setPixelColor(i, c);
+      strip.show();
+      delay(wait);
+  }
+  Serial.println("color wipe -> complete");  
 }
--- bluelight_remote/bluelight_remote.ino
+++ bluelight_remote/bluelight_remote.ino
@@ -19,7 +19,6 @@
 #include "Adafruit_BLE.h"
 #include "Adafruit_BluefruitLE_SPI.h"
 #include "Adafruit_BluefruitLE_UART.h"
-
 #include "bluelight.h"
 // #include "BluefruitConfig.h"
 
@@ -33,53 +32,8 @@
 
 #include <Adafruit_NeoPixel.h>
 
-/*=========================================================================
-    APPLICATION SETTINGS
-
-    FACTORYRESET_ENABLE       Perform a factory reset when running this sketch
-   
-                              Enabling this will put your Bluefruit LE module
-                              in a 'known good' state and clear any config
-                              data set in previous sketches or projects, so
-                              running this at least once is a good idea.
-   
-                              When deploying your project, however, you will
-                              want to disable factory reset by setting this
-                              value to 0.  If you are making changes to your
-                              Bluefruit LE device via AT commands, and those
-                              changes aren't persisting across resets, this
-                              is the reason why.  Factory reset will erase
-                              the non-volatile memory where config data is
-                              stored, setting it back to factory default
-                              values.
-       
-                              Some sketches that require you to bond to a
-                              central device (HID mouse, keyboard, etc.)
-                              won't work at all with this feature enabled
-                              since the factory reset will clear all of the
-                              bonding data stored on the chip, meaning the
-                              central device won't be able to reconnect.
-    MINIMUM_FIRMWARE_VERSION  Minimum firmware version to have some new features
-    MODE_LED_BEHAVIOUR        LED activity, valid options are
-                              "DISABLE" or "MODE" or "BLEUART" or
-                              "HWUART"  or "SPI"  or "MANUAL"
-    PIN                       Which pin on the Arduino is connected to the NeoPixels?
-    NUMPIXELS                 How many NeoPixels are attached to the Arduino?
-    -----------------------------------------------------------------------*/
-    #define FACTORYRESET_ENABLE         1
-    #define MINIMUM_FIRMWARE_VERSION    "0.6.6"
-    #define MODE_LED_BEHAVIOUR          "MODE"
-    #define PIN                     6
-    #define NUMPIXELS               91
-    #define MICPIN                  A1
-    // Added to support RGB LED strips
-    #define REDPIN 11
-    #define GREENPIN 9
-    #define BLUEPIN 5
-    #define FADESPEED 5     // make this higher to slow down
-/*=========================================================================*/
 // Using strip instead, below is for NeoPixel pixel object
-// Adafruit_NeoPixel pixel = Adafruit_NeoPixel(NUMPIXELS, PIN);
+// Adafruit_NeoPixel pixel = Adafruit_NeoPixel(N_PIXELS, PIN);
 
 // Create the bluefruit object, either software serial...uncomment these lines
 /*
@@ -110,18 +64,21 @@ void error(const __FlashStringHelper*err) {
 // function prototypes over in packetParser.cpp
 // Input a value 0 to 255 to get a color value.
 // The colours are a transition r - g - b - back to r.
+/**
 uint32_t Wheel(byte WheelPos) {
   WheelPos = 255 - WheelPos;
   if(WheelPos < 85) {
-    return pixel.Color(255 - WheelPos * 3, 0, WheelPos * 3);
+    return strip.Color(255 - WheelPos * 3, 0, WheelPos * 3);
   }
   if(WheelPos < 170) {
     WheelPos -= 85;
-    return pixel.Color(0, WheelPos * 3, 255 - WheelPos * 3);
+    return strip.Color(0, WheelPos * 3, 255 - WheelPos * 3);
   }
   WheelPos -= 170;
-  return pixel.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
+  return strip.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
 }
+**/
+
 uint8_t readPacket(Adafruit_BLE *ble, uint16_t timeout);
 float parsefloat(uint8_t *buffer);
 void printHex(const uint8_t * data, const uint32_t numBytes);
@@ -141,7 +98,7 @@ extern uint8_t packetbuffer[];
 uint8_t red = 100;
 uint8_t green = 100;
 uint8_t blue = 100;
-uint8_t animationState = 1;
+uint8_t animationState = 0;
 uint8_t mode = 1;
 bool debug = false;
 
@@ -168,13 +125,13 @@ int
 //   NEO_GRB     Pixels are wired for GRB bitstream (most NeoPixel products)
 //   NEO_RGB     Pixels are wired for RGB bitstream (v1 FLORA pixels, not v2)
 // Adafruit_NeoPixel strip = Adafruit_NeoPixel(150, PIN, NEO_GRB + NEO_KHZ800);
-Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB);
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(N_PIXELS, LED_PIN, NEO_GRB);
 
 void setup(void)
 {
   Serial.println("setup -> start");
   // Configure pin for onboard LED blinking
-  pinMode(13, OUTPUT);
+  pinMode(BOARD_PIN, OUTPUT);
   // Setup for RGB LED strip
   // Commenting out since using Neopixel strip
   /**
@@ -191,7 +148,7 @@ void setup(void)
   // turn off neopixel
   /*
   pixel.begin(); // This initializes the NeoPixel library.
-  for(uint8_t i=0; i<NUMPIXELS; i++) {
+  for(uint8_t i=0; i<N_PIXELS; i++) {
     pixel.setPixelColor(i, pixel.Color(0,0,0)); // off
   }
   colorWipe(pixel.Color(100, 100, 100), 20); // do a quick colorWipe to show that the pixels are all working, even before Bluefruit connection established
@@ -283,13 +240,14 @@ void setup(void)
 /**************************************************************************/
 void loop(void)
 {
-  digitalWrite(13, HIGH);   // turn the LED on (HIGH is the voltage level)
+  digitalWrite(BOARD_PIN, HIGH);   // turn the LED on (HIGH is the voltage level)
   // delay(1000);              // wait for a second
   Serial.println("loop() top");
   Serial.print("animationState = ");
   Serial.println(animationState);
   // Check for user input
   char inputs[BUFSIZE+1];
+  
 /**
   if ( getUserInput(inputs, BUFSIZE) )
   {
@@ -367,65 +325,89 @@ void loop(void)
   if (strcmp(ble.buffer, "14") == 0) {
       Serial.println("Setting animationState = 14");
       animationState = 14;
-  }  
+  }
+  if (strcmp(ble.buffer, "15") == 0) {
+      Serial.println("Setting animationState = 15");
+      animationState = 15;
+  }
 
   Serial.print("animationState = ");
   Serial.println(animationState);
   if (animationState == 0){
-    pixel.begin(); // This initializes the NeoPixel library.
-    for(uint8_t i=0; i<NUMPIXELS; i++) {
-      pixel.setPixelColor(i, pixel.Color(0,0,0)); // off
+    strip.begin(); // This initializes the NeoPixel library.
+    for(uint8_t i=0; i<N_PIXELS; i++) {
+      strip.setPixelColor(i, strip.Color(0,0,0)); // off
     }
    }
 
    if (animationState == 1){
-     colorWipe(pixel.Color(100, 100, 100), 20); // do a quick colorWipe to show that the pixels are all working, even before Bluefruit connection established
-     colorWipe(pixel.Color(0, 0, 0), 20);
-     pixel.show();
+     colorWipe(strip.Color(100, 100, 100), 20); // do a quick colorWipe to show that the pixels are all working, even before Bluefruit connection established
+     colorWipe(strip.Color(0, 0, 0), 20);
+     strip.show();
     }
 
   if (animationState == 10){
-    for(uint16_t i=0; i<pixel.numPixels(); i++) { //clear all pixels before displaying new animation
-          pixel.setPixelColor(i, pixel.Color(0,0,0));
+    for(uint16_t i=0; i<strip.numPixels(); i++) { //clear all pixels before displaying new animation
+          strip.setPixelColor(i, strip.Color(0,0,0));
         }
      flashRandom(1,random(10,30));
-     pixel.show(); // This sends the updated pixel color to the hardware.
+     strip.show(); // This sends the updated pixel color to the hardware.
    }
 
   if (animationState == 11){
-    for(uint16_t i=0; i<pixel.numPixels(); i++) { //clear all pixels before displaying new animation
-          pixel.setPixelColor(i, pixel.Color(0,0,0));
+    colorWipe(strip.Color(255, 0, 0), DELAY); // Red
+    colorWipe(strip.Color(0, 255, 0), DELAY); // Green
+    colorWipe(strip.Color(0, 0, 255), DELAY); // Blue
+    /**
+    for(uint16_t i=0; i<strip.numPixels(); i++) { //clear all pixels before displaying new animation
+          Serial.println("animation state = 11a");
+          Serial.print(strip.numPixels());
+          strip.setPixelColor(i, strip.Color(0,0,0));
         }
-    colorWipe(pixel.Color(red, green, blue), 20);
-    pixel.show(); // This sends the updated pixel color to the hardware.
-    colorWipe(pixel.Color(0, 0, 0), 20);
-    pixel.show();
+    Serial.println("animation state = 11b");
+    Serial.print(red);
+    Serial.print(green);
+    Serial.print(blue);    
+    colorWipe(strip.Color(red, green, blue), 20);
+    // strip.show(); // This sends the updated pixel color to the hardware.
+    // colorWipe(strip.Color(0, 0, 0), 20);
+    strip.show();
+    **/
   }
 
   if (animationState == 12){
-    for(uint16_t i=0; i<pixel.numPixels(); i++) { //clear all pixels before displaying new animation
-          pixel.setPixelColor(i, pixel.Color(0,0,0));
+    for(uint16_t i=0; i<strip.numPixels(); i++) { //clear all pixels before displaying new animation
+          strip.setPixelColor(i, strip.Color(0,0,0));
         }
-    larsonScanner(30); // larsonScanner is set to red and does not take color input.
-    pixel.show(); // This sends the updated pixel color to the hardware.
+    larsonScanner(15); // larsonScanner is set to red and does not take color input.
+    strip.show(); // This sends the updated pixel color to the hardware.
   }
 
   if (animationState == 13){
-    for(uint16_t i=0; i<pixel.numPixels(); i++) { //clear all pixels before displaying new animation
-          pixel.setPixelColor(i, pixel.Color(0,0,0));
+    for(uint16_t i=0; i<strip.numPixels(); i++) { //clear all pixels before displaying new animation
+          strip.setPixelColor(i, strip.Color(0,0,0));
         }
     rainbowCycle(20);
-    pixel.show(); // This sends the updated pixel color to the hardware.
+    strip.show(); // This sends the updated pixel color to the hardware.
   }
 
   if (animationState == 14){
-    rgb();
+    rainbow(DELAY);
   }
+
+  if (animationState == 15){
+    theaterChaseRainbow(DELAY);
+  }
+  /**
+  if (animationState == 15){
+    theaterChase();
+  }
+  **/
   
   Serial.println("loop() bottom");
   Serial.print("animationState = ");
   Serial.println(animationState);
-  digitalWrite(13, LOW);    // turn the LED off by making the voltage LOW
+  digitalWrite(BOARD_PIN, LOW);    // turn the LED off by making the voltage LOW
   // delay(1000);              // wait for a second
   ble.waitForOK();
 }
@@ -434,9 +416,12 @@ void loop(void)
 void colorWipe(uint32_t c, uint8_t wait) {
   Serial.println("  color wipe -> start");
   for(uint16_t i=0; i<strip.numPixels(); i++) {
+      Serial.print("  color wipe -> loop");    
+      Serial.println(strip.numPixels());
+      Serial.println(i);      
       strip.setPixelColor(i, c);
       strip.show();
-      delay(wait);
+      delay(10);
   }
   Serial.println("  color wipe -> complete");  
 }
@@ -444,29 +429,29 @@ void colorWipe(uint32_t c, uint8_t wait) {
 void larsonScanner(uint8_t wait){
    int j;
 
- for(uint16_t i=0; i<pixel.numPixels()+5; i++) {
+ for(uint16_t i=0; i<strip.numPixels()+5; i++) {
   // Draw 5 pixels centered on pos.  setPixelColor() will clip any
   // pixels off the ends of the strip, we don't need to watch for that.
-  pixel.setPixelColor(pos - 2, 0x100000); // Dark red
-  pixel.setPixelColor(pos - 1, 0x800000); // Medium red
-  pixel.setPixelColor(pos    , 0xFF3000); // Center pixel is brightest
-  pixel.setPixelColor(pos + 1, 0x800000); // Medium red
-  pixel.setPixelColor(pos + 2, 0x100000); // Dark red
+  strip.setPixelColor(pos - 2, 0x100000); // Dark red
+  strip.setPixelColor(pos - 1, 0x800000); // Medium red
+  strip.setPixelColor(pos    , 0xFF3000); // Center pixel is brightest
+  strip.setPixelColor(pos + 1, 0x800000); // Medium red
+  strip.setPixelColor(pos + 2, 0x100000); // Dark red
 
-  pixel.show();
+  strip.show();
   delay(wait);
 
   // Rather than being sneaky and erasing just the tail pixel,
   // it's easier to erase it all and draw a new one next time.
-  for(j=-2; j<= 2; j++) pixel.setPixelColor(pos+j, 0);
+  for(j=-2; j<= 2; j++) strip.setPixelColor(pos+j, 0);
 
   // Bounce off ends of strip
   pos += dir;
   if(pos < 0) {
     pos = 1;
     dir = -dir;
-  } else if(pos >= pixel.numPixels()) {
-    pos = pixel.numPixels() - 2;
+  } else if(pos >= strip.numPixels()) {
+    pos = strip.numPixels() - 2;
     dir = -dir;
   }
  }
@@ -476,7 +461,7 @@ void flashRandom(int wait, uint8_t howmany) {
  randomSeed(analogRead(0));
   for(uint16_t i=0; i<howmany; i++) {
     // get a random pixel from the list
-    int j = random(pixel.numPixels());
+    int j = random(strip.numPixels());
 
     // now we will 'fade' it in 5 steps
     for (int x=0; x < 5; x++) {
@@ -484,8 +469,8 @@ void flashRandom(int wait, uint8_t howmany) {
       int g = green * (x+1); g /= 5;
       int b = blue * (x+1); b /= 5;
 
-      pixel.setPixelColor(j, pixel.Color(r, g, b));
-      pixel.show();
+      strip.setPixelColor(j, strip.Color(r, g, b));
+      strip.show();
       delay(wait);
     }
     // & fade out in 5 steps
@@ -494,14 +479,15 @@ void flashRandom(int wait, uint8_t howmany) {
       int g = green * x; g /= 5;
       int b = blue * x; b /= 5;
 
-      pixel.setPixelColor(j, pixel.Color(r, g, b));
-      pixel.show();
+      strip.setPixelColor(j, strip.Color(r, g, b));
+      strip.show();
       delay(wait);
     }
   }
   // LEDs will be off when done (they are faded to 0)
 }
 
+/**
 void rainbowCycle(uint8_t wait) {
   Serial.println("  rainbowCycle -> start");
   uint16_t i, j;
@@ -515,6 +501,7 @@ void rainbowCycle(uint8_t wait) {
   }
   Serial.println("  rainbowCycle -> complete");  
 }
+**/
 
 void rainbow(uint8_t wait) {
   Serial.println("  rainbow -> start");         
@@ -709,7 +696,7 @@ void listen() {
 
 // Input a value 0 to 255 to get a color value.
 // The colors are a transition r - g - b - back to r.
-/**
+
 uint32_t Wheel(byte WheelPos) {
   if(WheelPos < 85) {
    return strip.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
@@ -721,4 +708,3 @@ uint32_t Wheel(byte WheelPos) {
    return strip.Color(0, WheelPos * 3, 255 - WheelPos * 3);
   }
 }
-**/

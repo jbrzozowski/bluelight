026619e97f88eb5fd50824957375e437e94d89da
John Jason Brzozowski <jjmb@jjmb.com>
2018-03-22 00:41:54 -0400

    New routine for listening

--- bluelight_remote/bluelight_remote.ino
+++ bluelight_remote/bluelight_remote.ino
@@ -25,6 +25,10 @@ any redistribution
   #include <SoftwareSerial.h>
 #endif
 
+#ifdef __AVR__
+#include <avr/power.h>   //Includes the library for power reduction registers if your chip supports them.
+#endif                   //More info: http://www.nongnu.org/avr-libc/user-manual/group__avr__power.htlm
+
 // BLE and Bluetooth variable and object definitions
 // @brief  Sets up the HW an the BLE module (this function is called automatically on startup)
 // Using strip instead, below is for NeoPixel pixel object
@@ -92,6 +96,14 @@ int
   lvl       = 10,      // Current "dampened" audio level
   minLvlAvg = 0,      // For dynamic adjustment of graph low & high
   maxLvlAvg = 512;
+
+  uint16_t gradient = 0; //Used to iterate and loop through each color palette gradually
+  uint8_t volume = 0;    //Holds the volume level read from the sound detector.
+  uint8_t last = 0;      //Holds the value of volume from the previous loop() pass.
+  float maxVol = 15;     //Holds the largest volume recorded thus far to proportionally adjust the visual's responsiveness.
+  float avgVol = 0;      //Holds the "average" volume-level to proportionally adjust the visual experience.
+  float avgBump = 0;     //Holds the "average" volume-change to trigger a "bump."
+  bool bump = false;     //Used to pass if there was a "bump" in volume
 
 // Setup
 void setup(void)
@@ -416,6 +428,41 @@ void loop(void) {
         // strip.show();
         // direct mode
         log("\tloop -> listen -> top\n");
+        volume = analogRead(MIC_PIN);       //Record the volume level from the sound detector
+        log("\tloop -> listen -> volume -> " + String(volume) + "\n");
+        avgVol = (avgVol + volume) / 2.0;     //Take our "average" of volumes.
+        log("\tloop -> listen -> avg_volume -> " + String(avgVol) + "\n");
+
+        //Sets a threshold for volume.
+        //  In practice I've found noise can get up to 15, so if it's lower, the visual thinks it's silent.
+        //  Also if the volume is less than average volume / 2 (essentially an average with 0), it's considered silent.
+        if (volume < avgVol / 2.0 || volume < 15) volume = 0;
+
+        //If the current volume is larger than the loudest value recorded, overwrite
+        if (volume > maxVol) maxVol = volume;
+
+        //This is where "gradient" is reset to prevent overflow.
+        if (gradient > 1529) {
+          log("\tloop -> listen -> gradient -> " + String(gradient) + "\n");
+          gradient %= 1530;
+          log("\tloop -> listen -> gradient -> " + String(gradient) + "\n");
+          //Everytime a palette gets completed is a good time to readjust "maxVol," just in case
+          //  the song gets quieter; we also don't want to lose brightness intensity permanently
+          //  because of one stray loud sound.
+          maxVol = (maxVol + volume) / 2.0;
+          log("\tloop -> listen -> max_volume -> " + String(maxVol) + "\n");
+        }
+
+        //If there is a decent change in volume since the last pass, average it into "avgBump"
+        if (volume - last > avgVol - last && avgVol - last > 0) avgBump = (avgBump + (volume - last)) / 2.0;
+        //if there is a notable change in volume, trigger a "bump"
+        bump = (volume - last) > avgBump;
+        pulse();        //Calls the visual to be displayed with the globals as they are.
+        gradient++;     //Increments gradient
+        last = volume;  //Records current volume for next pass
+        // delay(30);   //Paces visuals so they aren't too fast to be enjoyable
+
+        /**
         uint32_t listenColor = red;
 
         n   = analogRead(MIC_PIN);            // Raw reading from mic
@@ -474,6 +521,7 @@ void loop(void) {
         if((maxLvl - minLvl) < TOP) maxLvl = minLvl + TOP;
           minLvlAvg = (minLvlAvg * 63 + minLvl) >> 6; // Dampen min/max levels
           maxLvlAvg = (maxLvlAvg * 63 + maxLvl) >> 6; // (fake rolling average)
+          **/
         log("\tloop -> listen -> bottom\n");
       }
       if (animationState == 136){
@@ -766,6 +814,93 @@ void listen(uint32_t c) {
     minLvlAvg = (minLvlAvg * 63 + minLvl) >> 6; // Dampen min/max levels
     maxLvlAvg = (maxLvlAvg * 63 + maxLvl) >> 6; // (fake rolling average)
   log("\tlisten -> bottom\n");
+}
+
+//Pulse from center of the strand
+void pulse() {
+  fade(0.75);   //Listed below, this function simply dims the colors a little bit each pass of loop()
+
+  //Advances the gradient to the next noticeable color if there is a "bump"
+  if (bump) gradient += 64;
+
+  //If it's silent, we want the fade effect to take over, hence this if-statement
+  if (volume > 0) {
+    uint32_t col = getRainbow(gradient); //Our retrieved 32-bit color
+
+    //These variables determine where to start and end the pulse since it starts from the middle of the strand.
+    //  The quantities are stored in variables so they only have to be computed once.
+    int start = LED_HALF - (LED_HALF * (volume / maxVol));
+    int finish = LED_HALF + (LED_HALF * (volume / maxVol)) + strip.numPixels() % 2;
+    //Listed above, LED_HALF is simply half the number of LEDs on your strand. ↑ this part adjusts for an odd quantity.
+
+    for (int i = start; i < finish; i++) {
+      //"damp" creates the fade effect of being dimmer the farther the pixel is from the center of the strand.
+      //  It returns a value between 0 and 1 that peaks at 1 at the center of the strand and 0 at the ends.
+      float damp = float(
+                     ((finish - start) / 2.0) -
+                     abs((i - start) - ((finish - start) / 2.0))
+                   )
+                   / float((finish - start) / 2.0);
+
+      //Sets the each pixel on the strand to the appropriate color and intensity
+      //  strand.Color() takes 3 values between 0 & 255, and returns a 32-bit integer.
+      //  Notice "knob" affecting the brightness, as in the rest of the visuals.
+      //  Also notice split() being used to get the red, green, and blue values.
+      strip.setPixelColor(i, strip.Color(
+                             split(col, 0) * pow(damp, 2.0),
+                             split(col, 1) * pow(damp, 2.0),
+                             split(col, 2) * pow(damp, 2.0)
+                           ));
+    }
+    //Sets the max brightness of all LEDs. If it's loud, it's brighter.
+    //  "knob" was not used here because it occasionally caused minor errors in color display.
+    strip.setBrightness(255.0 * pow(volume / maxVol, 2));
+  }
+  //This command actually shows the lights. If you make a new visualization, don't forget this!
+  strip.show();
+}
+
+//Fades lights by multiplying them by a value between 0 and 1 each pass of loop().
+void fade(float damper) {
+  //"damper" must be between 0 and 1, or else you'll end up brightening the lights or doing nothing.
+  if (damper >= 1) damper = 0.99;
+  for (int i = 0; i < strip.numPixels(); i++) {
+    //Retrieve the color at the current position.
+    uint32_t col = (strip.getPixelColor(i)) ? strip.getPixelColor(i) : strip.Color(0, 0, 0);
+    //If it's black, you can't fade that any further.
+    if (col == 0) continue;
+    float colors[3]; //Array of the three RGB values
+    //Multiply each value by "damper"
+    for (int j = 0; j < 3; j++) colors[j] = split(col, j) * damper;
+    //Set the dampened colors back to their spot.
+    strip.setPixelColor(i, strip.Color(colors[0] , colors[1], colors[2]));
+  }
+}
+
+uint8_t split(uint32_t color, uint8_t i ) {
+  //0 = Red, 1 = Green, 2 = Blue
+  if (i == 0) return color >> 16;
+  if (i == 1) return color >> 8;
+  if (i == 2) return color >> 0;
+  return -1;
+}
+
+//This function simply take a value and returns a gradient color
+//  in the form of an unsigned 32-bit integer
+
+//The gradient returns a different, changing color for each multiple of 255
+//  This is because the max value of any of the 3 LEDs is 255, so it's
+//  an intuitive cutoff for the next color to start appearing.
+//  Gradients should also loop back to their starting color so there's no jumps in color.
+
+uint32_t getRainbow(unsigned int i) {
+  if (i > 1529) return getRainbow(i % 1530);
+  if (i > 1274) return strip.Color(255, 0, 255 - (i % 255));   //violet -> red
+  if (i > 1019) return strip.Color((i % 255), 0, 255);         //blue -> violet
+  if (i > 764) return strip.Color(0, 255 - (i % 255), 255);    //aqua -> blue
+  if (i > 509) return strip.Color(0, 255, (i % 255));          //green -> aqua
+  if (i > 255) return strip.Color(255 - (i % 255), 255, 0);    //yellow -> green
+  return strip.Color(255, i, 0);                               //red -> yellow
 }
 
 // @brief  Checks for user input (via the Serial Monitor)
